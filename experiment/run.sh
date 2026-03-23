8ab0eeedb1ada2159ff00137ca40c8372adb2016
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-23 21:42:07 +0100

    experiment: add --otfs=2 for on-the-fly subsumption
    time to solution: 18.58s (prev: 18.60s)

--- experiment/run.sh
+++ experiment/run.sh
@@ -11,6 +11,7 @@ $clingo \
     --opt-heuristic=model \
     --del-max=1000000 \
     --del-glue=5,0 \
+    --otfs=2 \
     --heuristic=Domain \
     --quiet=2,0,0 \
     $experiment/concretize.lp \
